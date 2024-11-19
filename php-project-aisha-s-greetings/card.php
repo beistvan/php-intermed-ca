1d1a87ac9aa30e0e640279905b42e3bbe5bfb51e
beistvan <beistvan@gmail.com>
2024-11-19 16:31:36 +0100

    Create card.php

---
+++ php-project-aisha-s-greetings/card.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '_header.php';
+require_once '_utilities.php';
+
+// Add your code here!
+
+?>
+
+<h1 class="my-4">Card Preview</h1>
+<pre class="bg-light p-3"><!-- Display the card content here! --></pre>
+
+<?php
+
+require_once '_footer.php';
+?>
